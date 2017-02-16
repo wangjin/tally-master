6b995296ce92f8ce95aae3d5109da4055768a228
wangjin <wangjin-252@hotmail.com>
2017-02-16 22:28:26 +0800

    增加matchers，修复登录页css、js、img被Spring Security阻止的问题

--- src/main/java/com/wangjin/tallymaster/config/WebSecurityConfig.java
+++ src/main/java/com/wangjin/tallymaster/config/WebSecurityConfig.java
@@ -19,17 +19,16 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
         http
                 .authorizeRequests()
-                // .antMatchers("/").permitAll()                      //请求路径"/"允许访问
-                .anyRequest().authenticated()                      //其它请求都需要校验才能访问
+                .antMatchers("/css/**", "/js/**", "/img/**").permitAll()                    //请求路径"/"允许访问
+                .anyRequest().authenticated()                                              //其它请求都需要校验才能访问
                 .and()
                 .formLogin()
-                .loginPage("/login")                             //定义登录的页面"/login"，允许访问
+                .loginPage("/login")                                                      //定义登录的页面"/login"，允许访问
                 .permitAll()
                 .and()
-                .logout()                                           //默认的"/logout", 允许访问
+                .logout()                                                                  //默认的"/logout", 允许访问
                 .permitAll()
                 .and()
                 .sessionManagement()
