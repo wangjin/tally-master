b3047ba5369f1f144d3596a2243853fc598017ab
wangjin <wangjin-252@hotmail.com>
2017-02-17 08:49:20 +0800

    修改包

--- src/main/java/com/wangjin/tallymaster/config/security/LoginAuthenticationProvider.java
+++ src/main/java/com/wangjin/tallymaster/config/security/LoginAuthenticationProvider.java
@@ -1,4 +1,4 @@
-package com.wangjin.tallymaster.config;
+package com.wangjin.tallymaster.config.security;
 
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
--- src/main/java/com/wangjin/tallymaster/config/security/WebSecurityConfig.java
+++ src/main/java/com/wangjin/tallymaster/config/security/WebSecurityConfig.java
@@ -1,4 +1,4 @@
-package com.wangjin.tallymaster.config;
+package com.wangjin.tallymaster.config.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
